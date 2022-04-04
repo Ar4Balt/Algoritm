bf6da5f0aa41835b798b1c511e43c234e691a4ce
tunnikovartur@gmail.com <tunnikovartur@gmail.com>
2022-04-04 07:37:45 +0300

    Нормальная работа

--- Algoritm/main 1_13.cpp
+++ Algoritm/main 1_13.cpp
@@ -100,3 +100,80 @@
 //	delete_matrix(matrix, width);
 //	return 0;
 //}
+
+#include <iostream>
+#include <iomanip>
+#define n 5
+#include <time.h>
+
+using namespace std;
+
+int max_number(int* mtr, int& max_i, int min = 1, int max = n) {
+	int number(0);
+	max_i = 0;
+	for (int i = min; i < max - 1; i++)
+	{
+		if (mtr[i] < mtr[i + 1]) {
+			number = mtr[i + 1];
+			max_i = i + 1;
+		}
+		else {
+			number = mtr[i];
+			max_i = i;
+		}
+	}
+	return number;
+}
+
+int main_1_13() {
+	//Для случайных чисел
+	//srand((unsigned)time(NULL));
+
+	int** mtr = new int* [n + 2];
+	int starting_point(0),
+		summa(0);
+	
+	cout << "Enter starting point (1 - " << n << "): ";
+	cin >> starting_point;
+	//Создание
+	for (int i = 0; i < n + 2; i++)
+	{
+		mtr[i] = new int[n + 2];
+		for (int j = 0; j < n + 2; j++)
+		{
+			if (i == 0 || i == n + 1 ||
+				j == 0 || j == n + 1){
+				mtr[i][j] = 0;
+			}
+			else {
+				mtr[i][j] = 1 + rand() % 20;
+			}
+		}
+	}
+
+	//Шаги
+	for (int i = 1; i < n + 1; i++)
+	{
+		int max_i(starting_point);
+		starting_point += max_number(mtr[i], max_i, starting_point - 1, starting_point + 1);
+		continue;
+	}
+
+	//Ввывод и удаление
+	for (int i = 1; i < n + 1; i++)
+	{
+		for (int j = 1; j < n + 1; j++)
+		{
+			cout << setw(3) << mtr[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl << endl << endl << starting_point;
+	for (int i = 0; i < n + 2; i++)
+	{
+		delete[] mtr[i];
+	}
+	delete[] mtr;
+
+	return 0;
+}
